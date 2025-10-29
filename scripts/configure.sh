47ea11c2aa62fbcbd2813fd943b54b012404c264
kgabriel-dev <gabrielkleebaum@yahoo.de>
2025-10-29 11:51:59 +0100

    improve configure.sh script

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # copy .xinitrc to the home directory
-cp ~/everyday-calendar/scripts/.xinitrc ~/.xinitrc
+cp everyday-calendar/scripts/.xinitrc .xinitrc
+chmod +x .xinitrc
 
 # install dependencies via apt
 sudo apt update
 sudo apt install -y python3 xserver-xorg xinit xterm x11-xserver-utils x11-utils chromium
-
-# add current user to the 'tty' group to allow access to the TTY (dev/tty2 is required for X server later)
-sudo usermod -aG tty $USER
-# reload the group memberships in order to apply the changes immediately
-newgrp tty
 
 # create the virtual environment for the backend and install dependencies
 python3 -m venv everyday-calendar/backend/.venv
 source everyday-calendar/backend/.venv/bin/activate
 pip install -r everyday-calendar/backend/requirements.txt
 deactivate
+
+# NOTE: the following commands need to be run at last because "newgrp tty" opens a new shell
+# add current user to the 'tty' group to allow access to the TTY (dev/tty2 is required for X server later)
+sudo usermod -aG tty $USER
+# reload the group memberships in order to apply the changes immediately
+newgrp tty
